d5ed601890c8ff13efd8f40cfecead14fe0182de
Roberto <robpalacios1@hotmail.com>
2019-05-27 23:43:12 -0500

    updateCount

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+	constructor() {
+	  super();
+	  this.state = {
+	  	count: 0
+	}
+}
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.updateCount.bind(this)}></textarea>
+        <div className="counter">{this.state.count}</div>
       </div>
     );
+  }
+
+  updateCount(e){
+  	this.setState({
+  		count: e.target.value.length
+  	});
   }
 }
 
